9c5c86544e935fb2191df7376f385c6806d2d8ba
Tamer Sarioglu <tamerthedark@hotmail.com>
2026-04-27 09:33:15 +0300

    feat: update Gradle configuration to support Java 21 and Kotlin JVM target 21

--- build-logic/convention/src/main/kotlin/com/shopscale/buildlogic/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/shopscale/buildlogic/AndroidFeatureConventionPlugin.kt
@@ -1,12 +1,15 @@
 package com.shopscale.buildlogic
 
 import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 
 class AndroidFeatureConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -24,6 +27,16 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 defaultConfig {
                     minSdk = 24
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_21
+                    targetCompatibility = JavaVersion.VERSION_21
+                }
+            }
+
+            extensions.configure<KotlinAndroidProjectExtension> {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_21)
                 }
             }
 
